0d69f78e6797c73c00f389adc80fd70c6684fd59
widcardw <7620969+widcardw@user.noreply.gitee.com>
2022-04-20 18:00:51 +0800

    Fix spaces in filepath of images

--- docs/.vuepress/plugin/double-bracket-media.js
+++ docs/.vuepress/plugin/double-bracket-media.js
@@ -8,13 +8,18 @@ module.exports = function (md) {
         if (ch !== 0x21 || pos >= max) { return false; }
 
         let text = state.src.substring(pos, max);
+        // 匹配 ![[public/(path/to/img.jpg)]]
         let rg = /^!\[\[public\/(.*?)\]\]/
         let match = text.match(rg);
 
         if (match && match.length) {
             let media = match[1];
+            // 判断是否存在文件后缀，有后缀的才是图片或视频
             if (!media.match(/(.+?)\.(.+?)/)) { return false; }
+            // 将路径中所有的空格都替换为 "%20"，这样就能保证路径带空格也能索引到媒体了
+            media = media.replace(/\s/g, "%20");
             let result = "";
+            // 匹配视频/音频/图片
             if (media.endsWith('.mp4')) {
                 result = `<video controls="controls" src="/${media}" style="width: 100%; height: 100%;"></video>`
             } else if (media.endsWith('.mp3')) {
@@ -24,6 +29,7 @@ module.exports = function (md) {
             }
             // console.log(result);
 
+            // markdown it 的处理操作
             token = state.push('paragraph_open', 'p', 1);
             token.markup = '@';
             token.map = [startLine, state.line];
