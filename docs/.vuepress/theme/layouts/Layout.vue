0d6b9224357e5392b85f21bc14d43551260b4c76
widcardw <7620969+widcardw@user.noreply.gitee.com>
2022-06-14 20:23:15 +0800

    fix: add giscus darkmode support

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,11 +1,13 @@
 <script setup>
 import ParentLayout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
+import { useDarkMode } from '@vuepress/theme-default/lib/client/composables/useDarkMode'
+const isDark = useDarkMode()
 </script>
 
 <template>
   <ParentLayout>
     <template #page-bottom>
-      <CommentService />
+      <CommentService :darkmode="isDark" />
     </template>
   </ParentLayout>
 </template>
